a1b7f8f22bc361506279999041992324532de7a1
Stopit-m8 <sunelsensurya@gmail.com>
2026-08-23 16:26:16 +0700

    Minigame 3 base

---
+++ Assets/Scripts/Minigame/Minigame3/Minigame3.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Minigame3 : MonoBehaviour, IMinigame
+{
+    public void StartMinigame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopMinigame()
+    {
+        throw new System.NotImplementedException();
+    }
+}
