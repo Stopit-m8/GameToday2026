8cb1d1ff517e4ec8971902db530fb10e7aefc6a5
Stopit-m8 <sunelsensurya@gmail.com>
2026-09-03 09:59:03 +0700

    minigame 2 animation + collider fix

--- Assets/Scripts/Minigame/Minigame2/BugClimb.cs
+++ Assets/Scripts/Minigame/Minigame2/BugClimb.cs
@@ -1,20 +1,27 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BugClimb : MonoBehaviour
 {
     public event Action<bool> OnArriveStart;
     [SerializeField] private Transform startPoint;
     [SerializeField] private Transform endPoint;
+    [SerializeField] private Image ballImage;
 
     [SerializeField] private float progressMultiplier = 0.001f;
 
     [Header("Read Only")]
     [SerializeField] private float progress;
-    
+
+    private void TurnBall(float spinAmount)
+    {
+        ballImage.rectTransform.Rotate(0f, 0f, -spinAmount);
+    }
 
     public void AddProgress(float spinAmount)
     {
+        TurnBall(spinAmount);
         progress += spinAmount * progressMultiplier;
         progress = Mathf.Clamp01(progress);
         transform.position = Vector3.Lerp(startPoint.position, endPoint.position, progress);
